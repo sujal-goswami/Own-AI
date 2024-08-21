d3e8d34de894b75c972d48bd78b69977049dda78
Sujal Goswami <sujalgoswamiofficial@gmail.com>
2024-08-21 18:48:04 +0530

    chore: Enable secure mode in Cloudinary configuration

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -6,6 +6,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
 });
 
 const uploadOnCloudinary = async (photoFile) => {
